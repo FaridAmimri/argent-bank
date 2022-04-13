805bd90827ca01e29b99fb522958914cc5757e3e
Farid Amimri <farid.amimri@gmail.com>
2022-04-13 18:07:40 +0000

    Ajout routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import User from './pages/User'
 
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
