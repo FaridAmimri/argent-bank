922878194f030600986d8f3aa63a6a8290cce6cf
Farid Amimri <farid.amimri@gmail.com>
2022-04-22 18:22:49 +0000

    Ajout call API getUserProfile

--- src/services.js
+++ src/services.js
@@ -1,13 +1,29 @@
 import axios from 'axios'
 
-export const loginUser = ({ email, password }) => {
+export const getUserLogin = ({ email, password }) => {
   return axios
     .post('http://localhost:3001/api/v1/user/login', {
       email,
       password,
     })
-    .catch(function (error) {
-      console.log(error)
-    })
+    .catch((error) => console.log(error))
 }
 
+export const getUserProfile = ({ token }) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  }
+
+  const bodyParameters = {
+    key: 'value',
+  }
+  return axios
+    .post('http://localhost:3001/api/v1/user/profile', bodyParameters, config)
+    .then((res) => {
+      return {
+        firstName: res.data.body.firstName,
+        lastName: res.data.body.lastName,
+      }
+    })
+    .catch((error) => console.log(error))
+}
