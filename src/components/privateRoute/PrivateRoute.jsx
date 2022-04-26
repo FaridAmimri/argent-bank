2bb2743eeae65fa47f7d2a193fec392dad7d894b
Farid Amimri <farid.amimri@gmail.com>
2022-04-26 13:50:57 +0000

    Ajout PrivateRoute

---
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { userSelector } from '../../features/userSlice'
+
+function PrivateRoute({ children, targetComponent: TargetComponent }) {
+  const user = useSelector(userSelector)
+  const token = user.token
+
+  if (token !== null) {
+    return children
+  }
+
+  return <Navigate to="/login" />
+}
+
+export default PrivateRoute
