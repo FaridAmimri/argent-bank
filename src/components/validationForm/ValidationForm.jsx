247dde4100aa6d485c5f0fa3bdbb1253e2bbc02d
Farid Amimri <farid.amimri@gmail.com>
2022-04-27 13:48:55 +0000

    Ajout composant pour validation formulaire

---
+++ src/components/validationForm/ValidationForm.jsx
@@ -0,0 +1,13 @@
+function ValidationForm(values) {
+  let errors = {}
+
+  if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = 'Email is invalid'
+  }
+  if (values.password.length < 10) {
+    errors.password = ' Password is invalid'
+  }
+  return errors
+}
+
+export default ValidationForm
